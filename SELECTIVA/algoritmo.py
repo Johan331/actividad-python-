3680fd24a5ccc04b27b80a3345d2b3ed7265b90b
santiago romero <romero.dsrv@gmail.com>
2024-03-09 10:08:26 -0500

    first commit

---
+++ SELECTIVA/algoritmo.py
@@ -0,0 +1,31 @@
+
+
+valor_compra = int(input("Digite el valor para la variable valor_compra: "))
+descuento = 0.05
+valor_total = valor_compra-(valor_compra*descuento)
+print("el valor de la compra es de ",valor_total)
+
+
+
+#segundo
+x= int(input("Digite el valor para la variable x: "))
+
+
+
+if x > 5:
+    print("x es mayor que 5")
+else: print("x no es mayor que 5)")
+
+#Segundo algoritmo
+descuento = 0
+
+if valor_compra  > 50000:
+    descuento = valor_compra * 0.10
+elif valor_compra==0:
+    print("No genera desceunto ")    
+elif valor_compra < 50000:
+    descuento = valor_compra * 0.05
+elif valor_compra == 50000:
+    descuento = valor_compra * 0.07
+
+
